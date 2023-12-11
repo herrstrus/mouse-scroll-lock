9d999e04469092594a9affe22cea4fac8a464d83
herrstrus <irek.prz@gmail.com>
2023-12-11 09:35:33 +0100

    fix teams name as there is new version available

--- keylock-mac.sh
+++ keylock-mac.sh
@@ -2,7 +2,7 @@
 caffeinate -d &
 while true;
 do
-	osascript -e 'tell application "Microsoft Teams" to activate'
+	osascript -e 'tell application "Microsoft Teams classic" to activate'
 	osascript -e 'tell application "System Events" to keystroke "2" using {command down}'
 	echo "Teams Status Refreshed"
 	sleep 300
